9a8dd4bc877c314dc3eaa2702fedc43ef3fce905
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-07-09 17:13:32 -0700

    lots of updates
    add filter input to header section
    
    change color scheme
    
    change styling to .page-title
    
    change modal overlay opacity
    
    add left and right buttons to modal
    
    add js for modal close on overlay click event
    
    update card and modal styling
    
    add animated underline to .name on cards
    
    add card and avatar hover effects
    
    made more responsive

--- js/app.js
+++ js/app.js
@@ -2,11 +2,13 @@
 let employees = [];
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
 email, location, phone, dob &noinfo &nat=US`;
-const gridContainer = document.querySelector(".grid-container");
-const overlay = document.querySelector(".overlay");
-const modalContainer = document.querySelector(".modal-content");
-const modalClose = document.querySelector(".modal-close");
+const gridContainer = document.querySelector('.grid-container');
+const overlay = document.querySelector('.overlay');
+const modalContainer = document.querySelector('.modal-content');
+const modalClose = document.querySelector('.modal-close');
 
+
+/* --------------- FETCH AND DISPLAY EMPLOYEE DATA ----------------- */
 
 // fetch data from API
 fetch(urlAPI)
@@ -25,20 +27,20 @@ function displayEmployees(employeeData) {
     let name = employee.name;
     let email = employee.email;
     let city = employee.location.city;
+    let state = employee.location.state;
     let picture = employee.picture;
     // template literals make this so much cleaner!
     employeeHTML += `
       <div class="card" data-index="${index}">
-        <img class="avatar" src="${picture.large}" />
+        <img class="avatar" src="${picture.large}" alt="${name.first} ${name.last}"/>
         <div class="text-container">
-          <h2 class="name">${name.first} ${name.last}</h2>
+          <h2 class="name hover-underline-animation">${name.first} ${name.last}</h2>
           <p class="email">${email}</p>
-          <p class="address">${city}</p>
+          <p class="address">${city}, ${state}</p>
         </div>
       </div>
     `;
   });
-
   gridContainer.innerHTML = employeeHTML;
 }
 
@@ -58,42 +60,199 @@ function displayModal(index) {
     },
     picture
   } = employees[index];
-
   let date = new Date(dob.date);
-
   const modalHTML = `
-    <img class="avatar" src="${picture.large}" />
+    <button id="left-btn">&Lang;</button>
+    <button id="right-btn">&Rang;</button>
+    <img class="avatar modal-avatar" src="${picture.large}" alt="${name.first} ${name.last}" / >
     <div class="text-container">
-      <h2 class="name">${name.first} ${name.last}</h2>
+      <h2 class="modal-name">${name.first} ${name.last}</h2>
       <p class="modal-email">${email}</p>
       <p class="address">${city}</p>
-      <hr />
-      <p>${phone}</p>
-      <p class="address">${street.number} ${street.name}, ${city}, ${state} ${postcode}</p>
-      <p>Birthday:
+      <hr class="modal-hr"/>
+      <p class="phone">${phone}</p>
+      <p class="address" >${street.number} ${street.name}, ${state} ${postcode}</p>
+      <p class="dob">Birthday:
         ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}
       </p>
     </div>
   `;
-
-  overlay.classList.remove("hidden");
-
+  overlay.classList.remove('hidden');
   modalContainer.innerHTML = modalHTML;
 }
 
 
-gridContainer.addEventListener('click', e => {
+gridContainer.addEventListener('click', (e) => {
   // make sure the click is not on the gridContainer itself
   if (e.target !== gridContainer) {
     // select the card element based on its proximity to actual element clicked
-    const card = e.target.closest(".card");
+    const card = e.target.closest('.card');
     const index = card.getAttribute('data-index');
-
     displayModal(index);
   }
 });
 
 
+/* --------------- CLOSE MODAL ON CLOSE BUTTON CLICK EVENT ----------------- */
+
 modalClose.addEventListener('click', () => {
-  overlay.classList.add("hidden");
+  overlay.classList.add('hidden');
 });
+
+
+/* --------------- CLOSE MODAL ON OVERLAY CLICK EVENT ----------------- */
+
+overlay.addEventListener("click", (e) => {
+  if (e.target === overlay) {
+    overlay.classList.add("hidden");
+  }
+});
+
+
+/* --------------- EMPLOYEE FILTER ----------------- */
+
+function searchUsers() {
+  let input = document.getElementById('search').value;
+  input = input.toUpperCase();
+  let users = document.getElementsByClassName('name');
+  let card = document.getElementsByClassName('card');
+  for (let i = 0; i < users.length; i++) {
+    if (!users[i].innerHTML.toUpperCase().includes(input)) {
+      card[i].style.display = "none";
+    } else {
+      card[i].style.display = "";
+    }
+  }
+}
+
+
+/* --------------- GO BACK AND FORTH BETWEEN EMPLOYEE DETAILS IN MODAL ----------------- */
+
+// let rightBtn = document.getElementById('right-btn');
+// let leftBtn = document.getElementById('left-btn');
+
+// rightBtn.addEventListener('click', (e) => {
+//   if (index >= employees.length - 1) {
+//     index = '-1';
+//   }
+//   index = parseInt(index);
+//   index++;
+//   index = index.toString();
+
+//   let {
+//     name: {
+//       first,
+//       last
+//     },
+//     dob,
+//     phone,
+//     email,
+//     location: {
+//       city,
+//       street: {
+//         number,
+//         name
+//       },
+//       state,
+//       postcode
+//     },
+//     picture
+//   } = employees[index];
+
+//   let date = new Date(dob.date);
+
+//   document.getElementById('avatar').src = `${picture.large}`;
+//   document.getElementById('name').innerHTML = `${name.first} ${name.last}`;
+//   document.getElementById('email').innerHTML = `${email}`;
+//   document.getElementById('city').innerHTML = `${city}`;
+//   document.getElementById('phone').innerHTML = `${phone}`;
+//   document.getElementById('address').innerHTML = `${street.number} ${street.name}, ${city}, ${state} ${postcode}`;
+//   document.getElementById('birthday').innerHTML = `Birthday: ${(date.getMonth() + 1)}/${date.getDate()}/${date.getFullYear()}`;
+// });
+
+
+// leftBtn.addEventListener('click', (e) => {
+
+//   if (index <= 0) {
+//     index = '12';
+//   }
+
+//   index = parseInt(index);
+//   index--;
+//   index = index.toString();
+
+//   let {
+//     name: {
+//       first,
+//       last
+//     },
+//     dob,
+//     phone,
+//     email,
+//     location: {
+//       city,
+//       street: {
+//         number,
+//         name
+//       },
+//       state,
+//       postcode
+//     },
+//     picture
+//   } = employees[index];
+
+//   let date = new Date(dob.date);
+
+//   document.getElementById('avatar').src = `${picture.large}`;
+//   document.getElementById('name').innerHTML = `${name.first} ${name.last}`;
+//   document.getElementById('email').innerHTML = `${email}`;
+//   document.getElementById('city').innerHTML = `${city}`;
+//   document.getElementById('phone').innerHTML = `${phone}`;
+//   document.getElementById('address').innerHTML = `${street.number} ${street.name}, ${city}, ${state} ${postcode}`;
+//   document.getElementById('birthday').innerHTML = `Birthday: ${(date.getMonth() + 1)}/${date.getDate()}/${date.getFullYear()}`;
+// });
+
+// gridContainer.addEventListener('click', e => {
+//   if (e.target !== gridContainer) {
+//     const card = e.target.closest(".card");
+//     const index = card.getAttribute('data-index');
+//     displayModal(index);
+//   }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const previousButton = document.getElementById(".left-btn");
+// const nextButton = document.getElementById(".right-btn");
+// let employeesIndex;
+
+// // previous card function
+// function previousCard() {
+//   if (employeesIndex > 0) {
+//     displayModal((employeesIndex -= 1));
+//   }
+// }
+
+// // next card function
+// function nextCard() {
+//   if (employeesIndex < 11) {
+//     displayModal((employeesIndex += 1));
+//   }
+// }
+
+// //event listeners
+// nextButton.addEventListener("click", nextCard);
+// previousButton.addEventListener("click", previousCard);
