65253820467e2e4991342d31c98ff13342284ab6
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-07-26 22:10:38 -0700

    Allow new employees to show on click of l/r modal buttons

--- js/app.js
+++ js/app.js
@@ -69,7 +69,9 @@ function displayModal(index) {
     },
     picture
   } = employees[index];
+
   let date = new Date(dob.date);
+
   const modalHTML = `
     <button id="left-btn">&Lang;</button>
     <button id="right-btn">&Rang;</button>
@@ -77,10 +79,10 @@ function displayModal(index) {
     <div class="text-container">
       <h2 class="modal-name">${name.first} ${name.last}</h2>
       <p class="modal-email">${email}</p>
-      <p class="address">${city}</p>
+      <p class="city">${city}</p>
       <hr class="modal-hr"/>
       <p class="phone">${phone}</p>
-      <p class="address" >${street.number} ${street.name}, ${state} ${postcode}</p>
+      <p class="modal-address" >${street.number} ${street.name}, ${state} ${postcode}</p>
       <p class="dob">Birthday:
         ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}
       </p>
@@ -88,6 +90,88 @@ function displayModal(index) {
   `;
   overlay.classList.remove('hidden');
   modalContainer.innerHTML = modalHTML;
+
+  let rightBtn = document.getElementById('right-btn');
+  let leftBtn = document.getElementById('left-btn');
+
+  rightBtn.addEventListener('click', (e) => {
+    if (index >= employees.length - 1) {
+      index = '-1';
+    }
+    index = parseInt(index);
+    index++;
+    index = index.toString();
+
+    let {
+      name: {
+        first,
+        last
+      },
+      dob,
+      phone,
+      email,
+      location: {
+        city,
+        street: {
+          number,
+          name
+        },
+        state,
+        postcode
+      },
+      picture
+    } = employees[index];
+
+    let date = new Date(dob.date);
+
+    document.querySelector('.modal-avatar').src = `${picture.large}`;
+    document.querySelector('.modal-name').innerHTML = `${first} ${last}`;
+    document.querySelector('.modal-email').innerHTML = `${email}`;
+    document.querySelector('.city').innerHTML = `${city}`;
+    document.querySelector('.phone').innerHTML = `${phone}`;
+    document.querySelector('.modal-address').innerHTML = `${number} ${name}, ${state} ${postcode}`;
+    document.querySelector('.dob').innerHTML = `Birthday: ${(date.getMonth() + 1)}/${date.getDate()}/${date.getFullYear()}`;
+  });
+
+
+  leftBtn.addEventListener('click', (e) => {
+    if (index <= 0) {
+      index = '12';
+    }
+    index = parseInt(index);
+    index--;
+    index = index.toString();
+
+    let {
+      name: {
+        first,
+        last
+      },
+      dob,
+      phone,
+      email,
+      location: {
+        city,
+        street: {
+          number,
+          name
+        },
+        state,
+        postcode
+      },
+      picture
+    } = employees[index];
+
+    let date = new Date(dob.date);
+
+    document.querySelector('.modal-avatar').src = `${picture.large}`;
+    document.querySelector('.modal-name').innerHTML = `${first} ${last}`;
+    document.querySelector('.modal-email').innerHTML = `${email}`;
+    document.querySelector('.city').innerHTML = `${city}`;
+    document.querySelector('.phone').innerHTML = `${phone}`;
+    document.querySelector('.modal-address').innerHTML = `${number} ${name}, ${state} ${postcode}`;
+    document.querySelector('.dob').innerHTML = `Birthday: ${(date.getMonth() + 1)}/${date.getDate()}/${date.getFullYear()}`;
+  });
 }
 
 
@@ -144,3 +228,4 @@ function searchUsers() {
       card[i].style.display = "";
     }
   }
+}
